82c5211694eb1335533064014ed42d294361346b
Felipe Leite <felipelopesleite@gmail.com>
2026-08-02 15:23:02 -0400

    adding new commands using the cobra-cli add functionality

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
+/*
+Copyright © 2026 Felpe Leite <EMAIL ADDRESS>
+*/
 package main
 
-import "fmt"
+import "github.com/FelipeLLeite/expense-tracker/cmd"
 
 func main() {
-	fmt.Println("Hello felipe")
+	cmd.Execute()
 }
